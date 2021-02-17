69d6b1ad9c59cd5e58cc519f6a94109dd1f28aae
Davide Marelli <davide.marelli@unimib.it>
2021-02-17 08:55:31 +0100

    fix gcp export, consider unit scale

--- operators/export_gcps.py
+++ operators/export_gcps.py
@@ -108,6 +108,7 @@ class SFMFLOW_OT_export_gcps(bpy.types.Operator):
         frame_end = scene.frame_end
         render_scale = scene.render.resolution_percentage / 100
         render_size = (int(scene.render.resolution_x * render_scale), int(scene.render.resolution_y * render_scale))
+        unit_scale = scene.unit_settings.scale_length
         #
         frame_backup = scene.frame_current
         camera_backup = scene.camera
@@ -132,13 +133,14 @@ class SFMFLOW_OT_export_gcps(bpy.types.Operator):
                     clip_end = camera.data.clip_end
                     #
                     for gcp in gcps:
-                        v = gcp.location
-                        ray_direction = (v - camera.location)
+                        gcp_pos = gcp.location * unit_scale
+                        camera_pos = camera.location * unit_scale
+                        ray_direction = (gcp_pos - camera_pos)
                         ray_direction.normalize()
                         #
-                        result, _, _, _, obj, _ = scene.ray_cast(view_layer, camera.location, ray_direction)
+                        result, _, _, _, obj, _ = scene.ray_cast(view_layer, camera_pos, ray_direction)
                         if result and obj is gcp:   # gcp is not occluded
-                            v_ndc = world_to_camera_view(scene, camera, v)  # gcp.matrix_world @ v)
+                            v_ndc = world_to_camera_view(scene, camera, gcp_pos)
                             if (0.0 < v_ndc.x < 1.0 and 0.0 < v_ndc.y < 1.0 and clip_start < v_ndc.z < clip_end):
                                 # gcp is in the view frustum
                                 # FIXME use float values for gcp image coordinates? or rounded are enough?

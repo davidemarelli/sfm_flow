3d8b73d21092aeacc9f3327ac79c3a280631a298
Davide Marelli <davide.marelli@unimib.it>
2021-02-22 15:43:14 +0100

    add camera presets

--- operators/camera_add.py
+++ operators/camera_add.py
@@ -1,6 +1,7 @@
 
 import logging
 from math import pi
+from typing import List, Tuple
 
 import bpy
 from mathutils import Vector
@@ -34,6 +35,86 @@ class SFMFLOW_OT_camera_add(bpy.types.Operator):
     )
 
     # ==============================================================================================
+    camera_preset: bpy.props.EnumProperty(
+        name="Camera preset",
+        description="Choose a camera type",
+        items=[
+            ("campreset.default", "Default preset", "Default SFM Flow camera parameters."),
+            ("campreset.nikon_D750", "Nikon D750", "Nikon D750 camera parameters."),
+            ("campreset.sony_a7III", "Sony a7 III", "Sony a7 III camera parameters."),
+        ],
+        default="campreset.default",
+        options={'SKIP_SAVE'}
+    )
+
+    CAMERA_PRESETS = {
+        "campreset.default": {
+            "resolutions": [(1920, 1080)],
+            "sensor_size": (36, 24),
+        },
+        "campreset.nikon_D750": {
+            "resolutions": [(6016, 4016), (4512, 3008), (3008, 2008), (5008, 3336), (3752, 2504),
+                            (3936, 2624), (2944, 1968), (1968, 1312)],
+            "sensor_size": (35.9, 24),
+        },
+        "campreset.sony_a7III": {
+            "resolutions": [(6000, 4000), (6000, 3376), (3936, 2624), (3936, 2216), (3008, 1688), (3008, 2000)],
+            "sensor_size": (35.8, 23.8),
+        }
+    }
+
+    # ==============================================================================================
+    def _get_render_resolution_items(self, context: bpy.types.Context) -> List[Tuple[str, str, str, int]]:   # pylint: disable=unused-argument
+        """Get the list of available resolutions of the current camera preset.
+
+        Arguments:
+            context {bpy.context} -- current context
+
+        Returns:
+            List[Tuple[str, str, str, int]] -- List of {EnumProperty} items
+        """
+        items = []
+        if self.camera_preset in SFMFLOW_OT_camera_add.CAMERA_PRESETS:
+            for i, res in enumerate(SFMFLOW_OT_camera_add.CAMERA_PRESETS[self.camera_preset]['resolutions']):
+                res_name = "{}x{}".format(res[0], res[1])
+                items.append(("imgres." + res_name, res_name, "", i))
+        items.append(("imgres.custom", "Custom resolution", "", len(items)))
+        return items
+
+    set_image_resolution: bpy.props.BoolProperty(
+        name="Set image resolution",
+        description="Set image resolution",
+        default=True,
+        options={'SKIP_SAVE'}
+    )
+
+    image_resolution: bpy.props.EnumProperty(
+        name="Image resolution",
+        description="Image resolution",
+        items=_get_render_resolution_items,
+        options={'SKIP_SAVE'}
+    )
+
+    # ==============================================================================================
+    custom_image_resolution_x: bpy.props.IntProperty(
+        name="Image resolution X",
+        description="Image resolution X",
+        min=1,
+        soft_max=15360,
+        subtype='PIXEL',
+        options={'SKIP_SAVE'}
+    )
+
+    custom_image_resolution_y: bpy.props.IntProperty(
+        name="Image resolution Y",
+        description="Image resolution Y",
+        min=1,
+        soft_max=8640,
+        subtype='PIXEL',
+        options={'SKIP_SAVE'}
+    )
+
+    # ==============================================================================================
     location: bpy.props.FloatVectorProperty(
         size=3,
         default=(0., 0., 0.),
@@ -50,8 +131,22 @@ class SFMFLOW_OT_camera_add(bpy.types.Operator):
         """Operator layout"""
         layout = self.layout
         row = layout.split(factor=0.33, align=True)
-        row.label(text="Animation type")
+        row.label(text="Camera type")
         row.prop(self, "camera_type", text="")
+        row = layout.split(factor=0.33, align=True)
+        row.label(text="Camera preset")
+        row.prop(self, "camera_preset", text="")
+        row = layout.split(factor=0.33, align=True)
+        row.prop(self, "set_image_resolution")
+        if self.set_image_resolution:
+            row.prop(self, "image_resolution", text="")
+            if self.image_resolution == "imgres.custom":
+                row = layout.split(factor=0.33, align=True)
+                row.separator_spacer()
+                row = row.row(align=True)
+                row.prop(self, "custom_image_resolution_x", text="")
+                row.label(text="", icon='X')
+                row.prop(self, "custom_image_resolution_y", text="")
         row = layout.split(factor=0.33, align=True)
         row.label(text="Location")
         row.prop(self, "location", text="")
@@ -86,6 +181,8 @@ class SFMFLOW_OT_camera_add(bpy.types.Operator):
             set -- enum set in {‘RUNNING_MODAL’, ‘CANCELLED’, ‘FINISHED’, ‘PASS_THROUGH’, ‘INTERFACE’}
         """
         self.location = context.scene.cursor.location
+        self.custom_image_resolution_x = context.scene.render.resolution_x
+        self.custom_image_resolution_y = context.scene.render.resolution_y
         #
         wm = context.window_manager
         return wm.invoke_props_dialog(self)
@@ -101,31 +198,33 @@ class SFMFLOW_OT_camera_add(bpy.types.Operator):
         properties = context.scene.sfmflow
         new_cameras = []
         #
+        sensor_size = SFMFLOW_OT_camera_add.CAMERA_PRESETS[self.camera_preset]['sensor_size']
+        #
         # ------------------------------------------------------------------------------------------
         if self.camera_type == "camtype.single":
-            camera = self._create_new_camera(location=self.location)
+            camera = self._create_new_camera(location=self.location, sensor_size=sensor_size)
             cameras_collection.objects.link(camera)
             new_cameras.append(camera)
         #
         # ------------------------------------------------------------------------------------------
         elif self.camera_type == "camtype.uav_1":
             camera = self._create_new_camera(name="UAV nadir", location=self.location,
-                                             rotation_euler=Vector((0., 0., 0.)))
+                                             rotation_euler=Vector((0., 0., 0.)), sensor_size=sensor_size)
             cameras_collection.objects.link(camera)
             new_cameras.append(camera)
         #
         # ------------------------------------------------------------------------------------------
         elif self.camera_type == "camtype.uav_5":
             camera_n = self._create_new_camera(name="UAV nadir", location=self.location,
-                                               rotation_euler=Vector((0., 0., 0.)))
+                                               rotation_euler=Vector((0., 0., 0.)), sensor_size=sensor_size)
             camera_f = self._create_new_camera(name="UAV forward", location=self.location,
-                                               rotation_euler=Vector((pi/4, 0., 0.)))
+                                               rotation_euler=Vector((pi/4, 0., 0.)), sensor_size=sensor_size)
             camera_b = self._create_new_camera(name="UAV backward", location=self.location,
-                                               rotation_euler=Vector((pi/4, 0., pi)))
+                                               rotation_euler=Vector((pi/4, 0., pi)), sensor_size=sensor_size)
             camera_l = self._create_new_camera(name="UAV left", location=self.location,
-                                               rotation_euler=Vector((pi/4, 0., pi/2)))
+                                               rotation_euler=Vector((pi/4, 0., pi/2)), sensor_size=sensor_size)
             camera_r = self._create_new_camera(name="UAV right", location=self.location,
-                                               rotation_euler=Vector((pi/4, 0., -pi/2)))
+                                               rotation_euler=Vector((pi/4, 0., -pi/2)), sensor_size=sensor_size)
             #
             cameras_collection.objects.link(camera_n)
             cameras_collection.objects.link(camera_f)
@@ -169,6 +268,17 @@ class SFMFLOW_OT_camera_add(bpy.types.Operator):
                 c_prop.camera = c
         properties.render_cameras_idx = idx_backup
         #
+        # set render image resolution
+        if self.set_image_resolution:
+            if self.image_resolution == "imgres.custom":
+                context.scene.render.resolution_x = self.custom_image_resolution_x
+                context.scene.render.resolution_y = self.custom_image_resolution_y
+            else:
+                # TODO get tuple from CAMERA_PRESETS
+                res = tuple(map(int, self.image_resolution.replace("imgres.", '').split('x')))
+                context.scene.render.resolution_x = res[0]
+                context.scene.render.resolution_y = res[1]
+                #
         context.view_layer.update()   # update matrix_world after rotation
         return {'FINISHED'}
 
@@ -178,7 +288,19 @@ class SFMFLOW_OT_camera_add(bpy.types.Operator):
 
     # ==============================================================================================
     def _create_new_camera(self, name: str = "Camera", location: Vector = Vector((0., 0., 0.)),
-                           rotation_euler: Vector = Vector((1.11, 0., 0.82))) -> bpy.types.Camera:
+                           rotation_euler: Vector = Vector((1.11, 0., 0.82)),
+                           sensor_size: Tuple[float, float] = (36., 24.)) -> bpy.types.Camera:
+        """Create a new camera object and its data block. The camera object will not be linked to the scene.
+
+        Keyword Arguments:
+            name {str} -- name of the new camera (default: {"Camera"})
+            location {Vector} -- 3D location of the new camera (default: {Vector((0., 0., 0.))})
+            rotation_euler {Vector} -- rotation of the new camera in radians (default: {Vector((1.11, 0., 0.82))})
+            sensor_size {Tuple[float, float]} -- sensor size of the new camera (default: {(36., 24.)})
+
+        Returns:
+            bpy.types.Camera --
+        """
         #
         # --- create camera data
         cam_data = bpy.data.cameras.new(name)
@@ -191,9 +313,9 @@ class SFMFLOW_OT_camera_add(bpy.types.Operator):
         cam_data.clip_start = 0.100
         cam_data.clip_end = 1000.0
         # sensor
-        cam_data.sensor_width = 32            # sensor width in millimeters
-        cam_data.sensor_height = 18           # sensor height in millimeters
-        cam_data.sensor_fit = 'HORIZONTAL'
+        cam_data.sensor_width = sensor_size[0]    # sensor width in millimeters
+        cam_data.sensor_height = sensor_size[1]   # sensor height in millimeters
+        cam_data.sensor_fit = 'HORIZONTAL' if sensor_size[0] > sensor_size[1] else 'VERTICAL'
         # depth of field
         cam_data.dof.aperture_fstop = 2.8
         cam_data.dof.aperture_blades = 0
